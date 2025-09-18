d917b1ffcb97c9aa47774c641da72351336428ec
leeminjxun <th554751@naver.com>
2025-09-18 14:18:25 +0900

    Init : 초기 설정

---
+++ src/main/java/com/ayu/dadokim/DadokimApplication.java
@@ -0,0 +1,13 @@
+package com.ayu.dadokim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DadokimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DadokimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ayu/dadokim/DadokimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayu.dadokim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DadokimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
